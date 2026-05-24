cd62fca5790dc60e5246e11984ea721aece029eb
Duy Nguyen <thanhduy4112005@gmail.com>
2026-05-24 11:15:45 +0700

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,14 @@ import authRoute from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import userRoute from "./routes/user.route.js"
 import { protectedRoute } from "./middlewares/auth.middleware.js";
+import cors from "cors"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 //Public routes
 app.use("/api/auth", authRoute)
 //private routes
